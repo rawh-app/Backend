08430d39df9f5925d78cabfedfae8d384cff5835
ahmedeisa10 <aeisa7123@gmail.com>
2025-11-05 05:42:28 +0200

    Initial commit

---
+++ RAWH.DAL/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RAWH.DAL
+{
+    public class ApplicationDbContext: IdentityDbContext<ApplicationUser>
+
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students {  get; set; }
+
+
+    }
+}
