44df4cf46e41c238d3eaf4285abab18f6b4f4921
Ahmed Noser <ahmednoser2222@gmail.com>
2025-11-05 05:58:13 +0200

    Edit In Program

--- RAWH.API/Program.cs
+++ RAWH.API/Program.cs
@@ -16,7 +16,7 @@ namespace RAWH.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
-
+            // Tamer Elgayar
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
--- RAWH.BLL/obj/Debug/net8.0/RAWH.BLL.AssemblyInfo.cs
+++ RAWH.BLL/obj/Debug/net8.0/RAWH.BLL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RAWH.BLL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7234aa071dba9cdbc698af4a27ea3e0553da9712")]
 [assembly: System.Reflection.AssemblyProductAttribute("RAWH.BLL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RAWH.BLL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
